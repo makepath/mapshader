9a7a893dd3fdfa7e20666d32c3788003393ffa10
Sapir Carlo Lastimoza-Dooley <sapirdooley@users.noreply.github.com>
2021-02-10 23:22:54 -0600

    reformat tests, all test complete except: test_to_ogc_tile_metadata, test_to_esri_tile_metadata, test_get_level_by_extent, test_get_tiles_by_extent

--- mapshader/tests/test_mercator.py
+++ mapshader/tests/test_mercator.py
@@ -1,24 +1,66 @@
 import pytest
 from mapshader.mercator import MercatorTileDefinition
-from mapshader.tests.data import DEFAULT_SOURCES_FUNCS
-from mapshader.core import create_agg
 import math
 
-ranges = ((-20037508.34, 20037508.34), (-20037508.34, 20037508.34))
-test_obj = MercatorTileDefinition(ranges[0], ranges[1])
+# Test Data
+default_ranges = ((-20037508.34, 20037508.34), (-20037508.34, 20037508.34))
+test_object = MercatorTileDefinition(default_ranges[0], default_ranges[1])
 
+# Test Parameters
+tile_params = {
+    'q1_tile': ((0, 0, 1), True),
+    'q2_tile': ((-1, 1, 1), False),
+    'q3_tile': ((-1, -1, 1), False),
+    'q4_tile': ((1, -1, 1), False),
+    'neg_z_val': ((1, 1, -1), False)
+}
 
+resolution_params = {
+    'int': (10, 152.8740565703525),
+    'float': (10.25, 128.551246155303)
+}
+
+resolution_extent_params = {
+    'int': (((0, 0, 150, 220), 20, 30), [5, 11]),
+    'float': (((0, 0, 150.5, 220.30), 20.5, 28.67), [5.2493896058597835, 10.746341463414634])
+}
+
+pixels_to_meters_params = {
+    'int': ((100, 200, 2), (-16123932.491798975, -12210356.643597951)),
+    'float': ((100.25, 200.3, 2.7), (-17622397.097840715, -15212104.032723146))
+}
+
+meters_to_pixels_params = {
+    'int': ((-16123932.491798975, -12210356.643597951, 2), (100.00000000000003, 200)),
+    'float': ((-17622397.097840715, -15212104.032723146, 2.7), (100.25000000000001, 200.3))
+}
+
+pixels_to_tile_params = {
+    'int': ((100, 200, 2), (0, 3)),
+    'float': ((100.25, 200.3, 2.7), (0, 5.498019170849885))
+}
+
+pixels_to_raster_params = {
+    'int': ((100, 200, 2), (100, 824)),
+    'float': ((100.25, 200.3, 2), (100.250, 823.7))
+}
+
+meters_to_tile_params = {
+    'int': ((-16123932.491798975, -12210356.643597951, 2), (0, 3)),
+    'float': ((-17622397.097840715, -15212104.032723146, 2.7), (0, 5.498019170849885))
+}
+
+# Tests
 def test_MercatorTileDefinition():
     # Test Inputs
-    assert isinstance(test_obj.x_range, tuple)
-    assert isinstance(test_obj.y_range, tuple)
-    assert isinstance(test_obj.max_zoom, int)
-    assert isinstance(test_obj.min_zoom, int)
-    assert isinstance(test_obj.x_origin_offset, float)
-    assert isinstance(test_obj.y_origin_offset, float)
-    assert isinstance(test_obj.initial_resolution, float)
+    assert isinstance(test_object.x_range, tuple)
+    assert isinstance(test_object.y_range, tuple)
+    assert isinstance(test_object.max_zoom, int)
+    assert isinstance(test_object.min_zoom, int)
+    assert isinstance(test_object.x_origin_offset, float)
+    assert isinstance(test_object.y_origin_offset, float)
+    assert isinstance(test_object.initial_resolution, float)
     # assert isinstance(test_obj.format, int)
-    
 
 def test_to_ogc_tile_metadata():
     pass
@@ -26,86 +68,119 @@ def test_to_ogc_tile_metadata():
 def test_to_esri_tile_metadata():
     pass
 
-tile_params = {
-    'q1_tile': ((0, 0, 1), (True, 2)),
-    'q2_tile': ((-1, 1, 1), (False, 2)),
-    'q3_tile': ((-1, -1, 1), (False, 2)),
-    'q4_tile': ((1, -1, 1), (False, 2)),
-    'neg_z_val': ((1, 1, -1), (False, 0.5))
-}
-
 @pytest.mark.parametrize('args, expected', list(tile_params.values()), ids = list(tile_params.keys()))
 def test_is_valid_tile(args, expected):
     # Test Inputs
-    for arg in args:
-        assert isinstance(arg, int)
-    assert isinstance(expected[0], bool)
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args:
+        assert isinstance(a, int)
 
-    if expected[0] == True:
-        assert (args[0] > 0 or args[0] <= expected[1]) or (args[1] > 0 or args[1] <= expected[1])
-    else:
-        assert (args[0] < 0 or args[0] >= expected[1]) or (args[1] < 0 or args[1] >= expected[1])
+    assert isinstance(expected, bool)
 
-    #Test Outputs
-    test_tile = test_obj.is_valid_tile(args[0], args[1], args[2])
-    assert test_tile == expected[0]
+    # Test Output
+    assert test_object.is_valid_tile(args[0], args[1], args[2]) == expected
 
-@pytest.mark.parametrize("z, expected", [(10, 1024)])
-def test__get_resolution(z, expected):
-    new_res = test_obj._get_resolution(z)
 
+@pytest.mark.parametrize('arg, expected', list(resolution_params.values()), ids = list(resolution_params.keys()))
+def test__get_resolution(arg, expected):
     # Test Inputs
-    assert isinstance(z, int)
-    assert expected == 2 ** z 
+    assert isinstance(arg, int) or isinstance(arg, float)
 
-    # Test Outputs
-    assert new_res == test_obj.initial_resolution / expected
+    assert isinstance(expected, int) or isinstance(expected, float)
 
-res_extent_params = {
-    'small': (((0, 0, 150, 220), 20, 30), (50, 11)),
-    'medium': (((0, 0, 1800, 1200), 20, 30), (60, 60)),
-    'large': (((0, 0, 3900, 2600), 20, 30), (130, 130))
-}
-@pytest.mark.parametrize('args, expected', list(res_extent_params.values()), ids = list(res_extent_params.keys()))
-def test_resolution_by_extent(args, expected):
-    new_res = test_obj.get_resolution_by_extent(args[0], args[1], args[2])
+    # Test Output
+    assert test_object._get_resolution(arg) == expected
 
+@pytest.mark.parametrize('args, expected', list(resolution_extent_params.values()), ids = list(resolution_extent_params.keys()))
+def test_get_resolution_by_extent(args, expected):
     # Test Inputs
-    assert isinstance(args[0], tuple)
-    for arg in args[0]:
-        assert isinstance(arg, int)
-    assert isinstance(args[1], int)
-    assert isinstance(args[2], int)
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args[1:2]:
+        assert isinstance(a, int) or isinstance(a, float)
+    assert isinstance(args[0], tuple) and len(args[0]) == 4
+    for a in args[0]:
+        assert isinstance(a, int) or isinstance(a, float)
 
-    for exp in expected:
-        assert isinstance(exp, int)
+    assert isinstance(expected, list) and len(expected) == 2
+    for e in expected:
+        assert isinstance(e, int) or isinstance(e, float)
 
-    # Test Outputs
-    assert isinstance(new_res, list)
-    for res in new_res:
-        assert isinstance(res, float)
+    # Test Output
+    assert test_object.get_resolution_by_extent(args[0], args[1], args[2]) == expected
 
+def test_get_level_by_extent():
+    pass
 
-#@pytest.mark.parametrize("extent, height, width", [((0,1024,0, 1024), 20, 30)])
-#def test_get_level_by_extent(extent, height, width):
-#    new_level = test_obj.get_level_by_extent(extent, height, width)
-
-    # Test Inputs 
-#    assert isinstance(extent, tuple)
-#   assert isinstance(height, int)
-#    assert isinstance(width, int)
-
-@pytest.mark.parametrize("px, py, level", [(354, 345, 5)])
-def test_pixels_to_meters(px, py, level):
-    meters = test_obj.pixels_to_meters(px, py, level)
-    res = test_obj._get_resolution(level)
-
+@pytest.mark.parametrize('args, expected', list(pixels_to_meters_params.values()), ids = list(pixels_to_meters_params.keys()))
+def test_pixels_to_meters(args, expected):
     # Test Inputs
-    assert isinstance(px, int)
-    assert isinstance(py, int)
-    assert isinstance(level, int)
-    assert isinstance(res, float)
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args:
+        assert isinstance(a, int) or isinstance(a, float)
 
-    # Test Outputs
-    assert meters[0] == (px * res) - test_obj.x_origin_offset
-    assert meters[1] == (py * res) - test_obj.y_origin_offset
+    assert isinstance(expected, tuple) and len(expected) == 2
+    for e in expected:
+        assert isinstance(e, int) or isinstance(e, float)
+
+    # Test Output
+    assert test_object.pixels_to_meters(args[0], args[1], args[2]) == expected
+
+@pytest.mark.parametrize('args, expected', list(meters_to_pixels_params.values()), ids = list(meters_to_pixels_params.keys()))
+def test_meters_to_pixels(args, expected):
+    # Test Inputs
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args:
+        assert isinstance(a, int) or isinstance(a, float)
+
+    assert isinstance(expected, tuple) and len(expected) == 2
+    for e in expected:
+        assert isinstance(e, int) or isinstance(e, float)
+
+    # Test Output
+    assert test_object.meters_to_pixels(args[0], args[1], args[2]) == expected
+
+@pytest.mark.parametrize('args, expected', list(pixels_to_tile_params.values()), ids = list(pixels_to_tile_params.keys()))
+def test_pixels_to_tile(args, expected):
+    # Test Inputs
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args:
+        assert isinstance(a, int) or isinstance(a, float)
+
+    assert isinstance(expected, tuple) and len(expected) == 2
+    for e in expected:
+        assert isinstance(e, int) or isinstance(e, float)
+
+    # Test Output
+    assert test_object.pixels_to_tile(args[0], args[1], args[2]) == expected
+
+@pytest.mark.parametrize('args, expected', list(pixels_to_raster_params.values()), ids = list(pixels_to_raster_params.keys()))
+def test_pixels_to_raster(args, expected):
+    # Test Inputs
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args[0:1]:
+        assert isinstance(a, int) or isinstance(a, float)
+    assert isinstance(args[2], int) 
+
+    assert isinstance(expected, tuple) and len(expected) == 2
+    for e in expected:
+        assert isinstance(e, int) or isinstance(e, float)
+
+    # Test Output
+    assert test_object.pixels_to_raster(args[0], args[1], args[2]) == expected
+
+@pytest.mark.parametrize('args, expected', list(meters_to_tile_params.values()), ids = list(meters_to_tile_params.keys()))
+def test_meters_to_tile(args, expected):
+    # Test Inputs
+    assert isinstance(args, tuple) and len(args) == 3
+    for a in args:
+        assert isinstance(a, int) or isinstance(a, float)
+
+    assert isinstance(expected, tuple) and len(expected) == 2
+    for e in expected:
+        assert isinstance(e, int) or isinstance(e, float)
+
+    # Test Output
+    assert test_object.meters_to_tile(args[0], args[1], args[2]) == expected
+
+def test_get_tiles_by_extent():
+    pass
