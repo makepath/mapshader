619b8dadd312f3b6f91407372742f2bce71935d6
chloe <chlochlo.lexa@gmail.com>
2021-04-08 10:32:19 -0400

    add pyproject and edit setup try to get examples in correct path; still not working correctly

--- setup.py
+++ setup.py
@@ -1,33 +1,72 @@
+import os
+import sys
+import shutil
 from setuptools import setup
-setup(name='mapshader',
-      use_scm_version={
-          "write_to": "mapshader/_version.py",
-          "write_to_template": '__version__ = "{version}"',
-          "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-      },
-      description='Simple Python GIS Web Services',
-      url='https://github.com/makepath/mapshader',
-      packages=['mapshader',
-                'mapshader.tests'],
-      install_requires=['xarray-spatial',
-                        'datashader',
-                        'geopandas',
-                        'click',
-                        'jinja2',
-                        'spatialpandas',
-                        'pytest',
-                        'tbb',
-                        'rtree',
-                        'rioxarray',
-                        'matplotlib',
-                        'descartes',
-                        'flask',
-                        'flask-cors>=3.0.10',
-                        'rasterio',
-                        'jupyter',
-                        'pyarrow'],
-      zip_safe=False,
-      classifiers=["Programming Language :: Python :: 3",
-                   "License :: OSI Approved :: MIT License",
-                   "Operating System :: OS Independent"],
-      include_package_data=True)
+import pyct.build
+
+examples = [
+]
+
+extras_require = {
+    'tests': [
+        'pytest',
+    ],
+    'examples': examples,
+}
+
+extras_require['doc'] = extras_require['examples'] + ['numpydoc']
+
+extras_require['all'] = sorted(set(sum(extras_require.values(), [])))
+
+setup_args = dict(
+    name='mapshader',
+    use_scm_version={
+        "write_to": "mapshader/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    },
+    description='Simple Python GIS Web Services',
+    url='https://github.com/makepath/mapshader',
+    install_requires=['xarray-spatial',
+                      'datashader',
+                      'geopandas',
+                      'click',
+                      'jinja2',
+                      'spatialpandas',
+                      'pytest',
+                      'tbb',
+                      'rtree',
+                      'rioxarray',
+                      'matplotlib',
+                      'descartes',
+                      'flask',
+                      'flask-cors>=3.0.10',
+                      'rasterio',
+                      'jupyter',
+                      'pyarrow',
+                      'pyct'],
+    extras_require=extras_require,
+    zip_safe=False,
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent"],
+    packages=['mapshader',
+              'mapshader.tests'
+              ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'mapshader = mapshader.__main__:main'
+            ]
+        },
+    )
+
+if __name__ == '__main__':
+    example_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+                                               'mapshader', 'examples')
+    if 'develop' not in sys.argv:
+        pyct.build.examples(example_path, __file__, force=True)
+    setup(**setup_args)
+  
+    if os.path.isdir(example_path):
+        shutil.rmtree(example_path)
