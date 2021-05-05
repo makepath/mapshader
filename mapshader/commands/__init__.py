2187a337ee939bd433bad90fec11d79d68e8bd18
giancastro <giancastrok@gmail.com>
2021-05-05 12:35:07 -0300

    solve branch conflits

--- mapshader/commands/__init__.py
+++ mapshader/commands/__init__.py
@@ -1,4 +1,17 @@
-from .tif_to_netcdf import tif_to_netcdf
+from pkg_resources import iter_entry_points as entry_points
+
+import click
+from click_plugins import with_plugins
 
 
-__all__ = ['tif_to_netcdf']
+@with_plugins(cmd for cmd in list(entry_points('mapshader.commands')))
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+def main(args=None):
+    '''
+    mapshader command line interface.
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    main()
