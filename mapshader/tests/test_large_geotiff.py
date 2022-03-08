a8466da05f521368e14bfa697b73860b1fe1ecb6
Ian Thomas <ianthomas23@gmail.com>
2022-03-08 14:13:25 +0000

    Test overview netcdf files exist

--- mapshader/tests/test_large_geotiff.py
+++ mapshader/tests/test_large_geotiff.py
@@ -66,30 +66,24 @@ def test_large_geotiff_create_overviews():
 
 
 @pytest.mark.large
-def test_large_geotiff_read_overviews():
+@pytest.mark.parametrize("z, shape", [
+    [0, (55, 256)],
+    [1, (228, 512)],
+    [2, (457, 1024)],
+    [3, (911, 2048)],
+    [4, (1819, 4094)],
+    [5, (3636, 8186)],
+    [6, (7269, 16371)],
+])
+def test_large_geotiff_overview_files(z, shape):
     directory = os.path.join("examples", "large_geotiff", "overviews")
-    for overview in range(7):
-        filename = os.path.join(directory, f"{overview}_band_data.nc")
-        if not os.path.isfile(filename):
-            pytest.xfail("Overviews do not exist")
+    filename = os.path.join(directory, f"{z}_band_data.nc")
+    assert os.path.isfile(filename)
 
-    yaml_file = os.path.join("examples", "large_geotiff.yaml")
-    with open(yaml_file, 'r') as f:
-        content = f.read()
-        config_obj = yaml.safe_load(content)
-        source_obj = config_obj['sources'][0]
+    ds = xr.open_dataset(filename, chunks=dict(y=512, x=512))
+    da = ds["band_data"]
+    assert da.dtype == np.float32
+    assert da.shape == shape
 
-    source = MapSource.from_obj(source_obj)
-    source = source.load()
-
-    for z in range(8):
-        x = 2**(z-1) - 1
-        if x < 0:
-            x = 0
-        y = 0 if z < 2 else round(2**(z-1.65))
-
-        img = render_map(source, x=x, y=y, z=z, height=256, width=256)
-        one_color_component = img & 0xff
-        min_, max_ = one_color_component.min(), one_color_component.max()
-        assert max_ > 0
-        assert min_ < max_
+    assert da.min().compute().item() >= 0.0
+    assert da.max().compute().item() <= 1.0
